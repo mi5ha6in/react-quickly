435194daf0ad6d1bdb5bc44bf7e58cfd87deaba5
mi5ha6in <mi5ha6in@gmail.com>
2021-11-21 01:37:48 +0300

    adds Star component

--- star-rating/src/components/StarRating.js
+++ star-rating/src/components/StarRating.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating() {
-  return [
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="grey" />,
-    <FaStar color="grey" />,
-  ];
+const createArray = (length) => [...Array(length)];
+
+const Star = ({ selected = false }) => (
+  <FaStar color={selected ? "red" : "grey"} />
+);
+
+export default function StarRating({ totalStar = 5 }) {
+  return createArray(totalStar).map((item, index) => <Star key={index} />);
 }
