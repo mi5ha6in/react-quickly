84485feb647f715cb53c9989c1685a3311001b68
mi5ha6in <mi5ha6in@gmail.com>
2021-11-27 21:33:50 +0300

    use all component

--- color-organizer/src/App.js
+++ color-organizer/src/App.js
@@ -1,3 +1,8 @@
-function App() {}
+import React, { useState } from "react";
+import colorData from "./color-data.json";
+import ColorList from "./ColorList.js";
 
-export default App;
+export default function App() {
+  const [colors] = useState(colorData);
+  return <ColorList colors={colors} />;
+}
