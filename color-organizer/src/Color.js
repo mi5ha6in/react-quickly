390ad05c25ef302125f32986e7fab687029d9639
mi5ha6in <mi5ha6in@gmail.com>
2021-11-29 22:28:52 +0300

    adds button remove

--- color-organizer/src/Color.js
+++ color-organizer/src/Color.js
@@ -1,9 +1,19 @@
 import StarRating from "./StarRating";
+import { FaTrash } from "react-icons/fa";
 
-export default function Color({ title, color, rating }) {
+export default function Color({
+  title,
+  color,
+  rating,
+  id,
+  onRemove = (f) => f,
+}) {
   return (
     <section>
       <h1>{title}</h1>
+      <button onClick={() => onRemove(id)}>
+        <FaTrash />
+      </button>
       <div style={{ height: 50, backgroundColor: color }} />
       <StarRating selectedStars={rating} />
     </section>
