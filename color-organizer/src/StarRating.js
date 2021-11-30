a807584e76ccd89c7b4faea0c833b8733a976fc9
mi5ha6in <mi5ha6in@gmail.com>
2021-11-30 23:33:24 +0300

    adds onRate

--- color-organizer/src/StarRating.js
+++ color-organizer/src/StarRating.js
@@ -7,13 +7,18 @@ const Star = ({ selected = false, onSelect = (f) => f }) => (
   <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 );
 
-export default function StarRating({ totalStar = 5, selectedStars }) {
+export default function StarRating({
+  totalStar = 5,
+  selectedStars,
+  onRate = (f) => f,
+}) {
   return (
     <>
       {createArray(totalStar).map((item, index) => (
         <Star
           key={index}
           selected={selectedStars > index}
+          onSelect={() => onRate(index + 1)}
         />
       ))}
       <p>
