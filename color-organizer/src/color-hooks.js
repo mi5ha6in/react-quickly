ecd89c86df562cb2cd6eaab3901efd4386eea31d
mi5ha6in <mi5ha6in@gmail.com>
2021-12-06 00:49:40 +0300

    fix mistake

--- color-organizer/src/color-hooks.js
+++ color-organizer/src/color-hooks.js
@@ -5,7 +5,7 @@ import colorData from "./color-data.json";
 const ColorContext = createContext();
 export const useColors = () => useContext(ColorContext);
 
-export default function ColorProvider({ children }) {
+export function ColorProvider({ children }) {
   const [colors, setColors] = useState(colorData);
 
   const addColor = (title, color) => {
