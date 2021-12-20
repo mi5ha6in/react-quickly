17630f339c742ee595bc3323ea20c0fe9c286c76
mi5ha6in <mi5ha6in@gmail.com>
2021-12-20 22:43:45 +0300

    fetch data with promise

--- data/src/index.js
+++ data/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+fetch(`https://api.github.com/users/mi5ha6in`)
+  .then((response) => response.json())
+  .then(console.log)
+  .catch(console.error);
 
 ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode></React.StrictMode>,
+  document.getElementById("root")
 );
-
-
