2b644853108b2362fd0fa653a5e85dfadbe3cd1b
mi5ha6in <mi5ha6in@gmail.com>
2021-12-23 23:53:56 +0300

    adds localstorage

--- data/src/index.js
+++ data/src/index.js
@@ -2,22 +2,33 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
-const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify.data);
+const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 
 function GitHubUser({ login }) {
-  const [data, setData] = useState();
+  const [data, setData] = useState(loadJSON(`user:${login}`));
+
+  useEffect(() => {
+    if (!data) return;
+    if (data.login === login) return;
+    const { name, avatar_url, location } = data;
+    saveJSON(`user:${login}`, {
+      name,
+      login,
+      avatar_url,
+      location
+    });
+  }, [data]);
 
   useEffect(() => {
     if (!login) return;
+    if (data && data.login === login) return;
     fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
+      .then(response => response.json())
       .then(setData)
       .catch(console.error);
   }, [login]);
 
-  if (data) {
-    return <pre>{JSON.stringify(data, null, 2)}</pre>;
-  }
+  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
 
   return null;
 }
