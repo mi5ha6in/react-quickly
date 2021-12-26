471733ac7040fdb973080d801c121f8cca95f36d
mi5ha6in <mi5ha6in@gmail.com>
2021-12-26 23:56:42 +0300

    adds tahoe peaks

--- data/src/index.js
+++ data/src/index.js
@@ -1,43 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
-const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+const tahoe_peaks = [
+  { name: "Freel Peak", elevation: 10891 },
+  { name: "Monument Peak", elevation: 10067 },
+  { name: "Pyramid Peak", elevation: 9983 },
+  { name: "Mt. Tallac", elevation: 9735 },
+];
 
-function GitHubUser({ login }) {
-  const [data, setData] = useState(loadJSON(`user:${login}`));
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then((data) => data.json())
-      .then(setData)
-      .then(setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if (loading) return <h1>loading...</h1>;
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (!data) return null;
-
+function App() {
   return (
-    <div className="githubUser">
-      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
-      <div>
-        <h1>{data.login}</h1>
-        {data.name && <p>{data.name}</p>}
-        {data.location && <p>{data.location}</p>}
-      </div>
-    </div>
+    <ul>
+      {tahoe_peaks.map((peak, i) => (
+        <li key={i}>
+          {peak.name} - {peak.elevation.toLocaleString()}
+        </li>
+      ))}
+    </ul>
   );
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GitHubUser login="mi5ha6in" />
-  </React.StrictMode>,
+  <React.StrictMode></React.StrictMode>,
   document.getElementById("root")
 );
