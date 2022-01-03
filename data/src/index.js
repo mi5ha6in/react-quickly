cbb1b613868c7477187d2f1008942da36fda92af
mi5ha6in <mi5ha6in@gmail.com>
2022-01-03 21:06:02 +0300

    clear

--- data/src/index.js
+++ data/src/index.js
@@ -1,38 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { FixedSizeList } from "react-window";
-import faker from "faker";
-
-const bigList = [...Array(5000)].map(() => ({
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  avatar: faker.image.image(),
-}));
-
-function App() {
-  const renderRow = ({ index, style }) => (
-    <div style={{ ...style, ...{ display: "flex" } }}>
-      <img src={bigList[index].avatar} alt={bigList[index].name} width={50} />
-      <p>
-        {bigList[index].name} - {bigList[index].email}
-      </p>
-    </div>
-  );
-  return (
-    <FixedSizeList
-      height={window.innerHeight}
-      width={window.innerWidth - 20}
-      itemCount={bigList.length}
-      itemSize={50}
-    >
-      {renderRow}
-    </FixedSizeList>
-  );
-}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
