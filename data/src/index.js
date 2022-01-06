9858962863d7631eebfcb53f3d4f2d85ea48593c
mi5ha6in <mi5ha6in@gmail.com>
2022-01-06 23:56:44 +0300

    use Fetch component

--- data/src/index.js
+++ data/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import GitHubUser from "./GitHubUser";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GitHubUser login={"mi5ha6in"} />
   </React.StrictMode>,
   document.getElementById("root")
 );
