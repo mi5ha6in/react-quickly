dfda496b05f7303a749b008fab5d23f3dc981249
mi5ha6in <mi5ha6in@gmail.com>
2022-01-09 21:23:31 +0300

    adds useCallback and useMemo

--- data/src/useIterator.js
+++ data/src/useIterator.js
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 export const useIterator = (items = [], initialIndex = 0) => {
   const [i, setIndex] = useState(initialIndex);
 
-  const prev = () => {
+  const prev = useCallback(() => {
     if (i === 0) return setIndex(items.length - 1);
     setIndex(i - 1);
-  };
+  }, [i]);
 
-  const next = () => {
+  const next = useCallback(() => {
     if (i === items.length - 1) return setIndex(0);
     setIndex(i + 1);
-  };
+  }, [i]);
 
-  return [items[i], prev, next];
+  const item = useMemo(() => items[i], [i]);
+
+  return [item || items[0], prev, next];
 };
